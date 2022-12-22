2d3b6e37c1b5767f2a84cceeb8f0ea50e781aee3
dac <dac@dypm.org>
2022-12-22 10:37:26 +0530

    piyush did this

---
+++ server_piyush.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var student={
+  rollno:101,
+  name:"Harry potter",
+  rank:1
+};
+app.get('/',(req, res)=>{
+  res.send(student);
+});
+app.listen(5000);
+console.log("server started at port 5000");
