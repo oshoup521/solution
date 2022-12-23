f68e645e0c396bde308f888ebe30b7c2c4b93633
juberjm <juberjm7298@gmail.com>
2022-12-23 17:47:31 +0530

    zubair changes

---
+++ databse/cpp1.sql
@@ -0,0 +1,53 @@
+create table credentials(
+login_id varchar(20) primary key,
+password varchar(20),
+questions varchar(20),
+hint varchar(50));
+
+create table student(
+roll_no int primary key,
+email varchar(20),
+name varchar(20),
+branch varchar(50),
+year int,
+ssc decimal(9,2),
+hsc decimal(9,2),
+diploma decimal(9,2),
+degree decimal(9,2),
+cgpa decimal(9,2),
+skills varchar(80),
+login_id varchar(20),
+constraint fk_crd foreign key (login_id) references credentials(login_id)
+on update cascade
+on delete cascade);
+
+create table company(
+companyID int primary key,
+name varchar(20),
+location varchar (20),
+profile varchar(20),
+vacancy varchar(20),
+job_profile varchar(20),
+interview_time varchar(10),
+black_list varchar(100));
+
+create table co_ordinator(
+co_id int primary key,
+email varchar(20),
+name varchar(20),
+experience varchar(200),
+expertise varchar(20));
+
+create table placement_record(
+reg_id int primary key,
+company varchar(20),
+ctc int,
+aptitude boolean,
+coding boolean,
+HR boolean,
+placement_status boolean);
+
+
+
+
+
